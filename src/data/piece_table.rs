bef04f4f79e313e3f3eb8d48451f6e7141595913
Darren Burns <darren.burns@fanduel.com>
2017-10-16 00:26:50 +0100

    Load file into piece table, create initial piec

--- src/data/piece_table.rs
+++ src/data/piece_table.rs
@@ -3,18 +3,20 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
-
+#[derive(Debug)]
 pub struct PieceTable {
     original_file: String,
     pieces: Vec<Piece>,
 }
 
+#[derive(Debug)]
 struct Piece {
     source: Source,
     start: usize,
     length: usize,
 }
 
+#[derive(Debug)]
 enum Source {
     Orig,
     Add
@@ -22,10 +24,10 @@ enum Source {
 
 impl PieceTable {
 
-    fn new(from_file: &mut File) -> PieceTable {
+    pub fn new(from_file: &mut File) -> PieceTable {
         let mut orig_buffer = String::new();
         from_file.read_to_string(&mut orig_buffer)
-                 .expect("Unable to read file!");
+                 .expect("Unable to read file contents!");
 
         let file_length = orig_buffer.len();
         PieceTable {
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,18 @@ mod view;
 
 use rustty::Terminal;
 
+use std::io::prelude::*;
+use std::fs::File;
+
 
 fn main() {    
     let mut term = view::sandbox::create_terminal();
+    
+    let mut file = File::open("test.txt")
+                        .expect("Unable to open file");
+
+    let piece_table = data::piece_table::PieceTable::new(&mut file);
+
     loop {
         view::sandbox::draw_terminal(&mut term);
 
