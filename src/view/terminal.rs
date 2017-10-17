fb17321c463d7fa37e018d06d3cfa1a66042dac5
Darren Burns <darren.burns@fanduel.com>
2017-10-17 21:24:07 +0100

    Add status line

--- src/view/terminal.rs
+++ src/view/terminal.rs
@@ -36,25 +36,31 @@ pub fn draw_cursor(term: &mut Terminal) {
 // to the terminal client as a vector of string slices.
 fn draw_editor_window(term: &mut Terminal, lines: Vec<&str>) {
     let terminal_height = term.size().1;
-    {
-        let last_visible_line_index = cmp::min(lines.len(), terminal_height);
-        let visible_lines = lines[..last_visible_line_index].into_iter();
 
-        for (y, line) in visible_lines.enumerate() {
-            let line_number = y + 1;  // TODO: Change it to y + 1 + scroll_offset when scrolling ready
+    let last_visible_line_index = cmp::min(lines.len(), terminal_height);
+    let visible_lines = lines[..last_visible_line_index].into_iter();
 
-            // Paint the gutter.
-            let gutter_cell = Cell::with_style(Color::Black, Color::Red, Attr::BoldUnderline);
-            let line_number_string = format!("{:>width$}", line_number.to_string(), width=GUTTER_WIDTH);
-            term.printline_with_cell(0, y, line_number_string.as_str(), gutter_cell);
+    for (y, line) in visible_lines.enumerate() {
+        let line_number = y + 1;  // TODO: Change it to y + 1 + scroll_offset when scrolling ready
 
-            // Paint the characters.
-            for (ch_idx, ch) in line.chars().enumerate() {
-                let screen_ch_idx = ch_idx + GUTTER_WIDTH + GUTTER_RIGHT_MARGIN;
-                term[(screen_ch_idx, y)].set_ch(ch);
-            }
+        // Paint the gutter.
+        let gutter_cell = Cell::with_style(Color::Black, Color::Red, Attr::BoldUnderline);
+        let line_number_string = format!("{:>width$}", line_number.to_string(), width=GUTTER_WIDTH);
+        term.printline_with_cell(0, y, line_number_string.as_str(), gutter_cell);
 
+        // Paint the characters.
+        for (ch_idx, ch) in line.chars().enumerate() {
+            let screen_ch_idx = ch_idx + GUTTER_WIDTH + GUTTER_RIGHT_MARGIN;
+            term[(screen_ch_idx, y)].set_ch(ch);
         }
     }
 
+    draw_status_line(term);
+}
+
+fn draw_status_line(term: &mut Terminal) {
+    let terminal_width  = term.size().0;
+    let terminal_height = term.size().1;
+    let status_string = format!("{file} - {x}, {y}", file="Somefile.txt", x=0, y=0);
+    term.printline(0, terminal_height - 1, status_string.as_str());
 }
